afb1c0bd749db345d4a2fd47ff95bc930ca2eaf7
Sadaf <sadafara@gmail.com>
2025-03-01 19:43:56 +0530

    homeDashboard

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,23 @@ import React from 'react'
 
 function Home() {
   return (
-    <Box mt={89}>Home</Box>
+    <>
+      <div className='homeDash'>
+        <div className='homeHead'>
+          <p>Home Dashboard</p>
+          <img src="https://toppng.com/uploads/preview/stock-person-png-stock-photo-man-11563049686zqeb9zmqjd.png" alt="" />
+        </div>
+        <div className='trendingPost'>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className='userProfile'>
+          <p>User Profiles</p>
+          
+        </div>
+      </div>
+    </>
   )
 }
 
