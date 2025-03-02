4dcc16759ca104b0b760eb6324c930bc2ff524a0
Sourav Roy <souravroy2145@gmail.com>
2025-03-02 20:55:30 +0530

    All code merge

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,9 @@ import { Route, Routes } from "react-router";
 import Register from "./components/Register";
 import NotFound from "./components/NotFound";
 import ProtectedRoutes from "./context/ProtectedRoutes";
-import Community from "./components/Community";
 import Profile from "./components/Profile";
 import PostCreator from "./components/PostCreator";
 import LandingPage from "./components/LandingPage";
-
-import Profile from "./components/Profile";
 import CommunityPage from "./components/CommunityPage";
 
 
@@ -20,17 +17,12 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/community" element={<Community />} />
-        <Route path="/post-creator" element={<PostCreator />} />
-        <Route path="/leading-page" element={<LandingPage />} />
-        <Route path="/community" element={<CommunityPage />} />
         <Route path="/" element={<LandingPage />} />
         <Route
           path="/community"
           element={
             <ProtectedRoutes>
-              <Community />
+              <CommunityPage />
             </ProtectedRoutes>
           }
         />
